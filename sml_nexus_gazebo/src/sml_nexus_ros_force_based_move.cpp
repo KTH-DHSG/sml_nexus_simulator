4c44ca48f817d3eb554c5d6d9ef004f94aa3a9d7
RBinsonB <r-baran@hotmail.fr>
2020-07-24 11:10:30 +0200

    Add y-axis linear velocity to odom twist

--- sml_nexus_gazebo/src/sml_nexus_ros_force_based_move.cpp
+++ sml_nexus_gazebo/src/sml_nexus_ros_force_based_move.cpp
@@ -343,6 +343,8 @@ namespace gazebo
     tf::poseTFToMsg(odom_transform_, odom_.pose.pose);
     odom_.twist.twist.angular.z = angular_vel.Z();
     odom_.twist.twist.linear.x  = linear_vel.X();
+    odom_.twist.twist.linear.y  = linear_vel.Y();
+
 #else
     math::Vector3 angular_vel = parent_->GetRelativeAngularVel();
     math::Vector3 linear_vel = parent_->GetRelativeLinearVel();
@@ -352,6 +354,7 @@ namespace gazebo
     tf::poseTFToMsg(odom_transform_, odom_.pose.pose);
     odom_.twist.twist.angular.z = angular_vel.z;
     odom_.twist.twist.linear.x  = linear_vel.x;
+    odom_.twist.twist.linear.y  = linear_vel.y;
 #endif
 
     odom_.header.stamp = current_time;
